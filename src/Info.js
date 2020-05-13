dca0cebca6a1a4c5a94b9b3cdbeb9b1cb8236872
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-05-13 23:48:31 +0900

    useEffect compare componentDidMount, componentDidUpdate

--- src/Info.js
+++ src/Info.js
@@ -1,11 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Info = () => {
 	const [name, setName] = useState('');
 	const [nickname, setNickname] = useState('');
 
+	// useEffect(() => {
+	// 	console.log('렌더링 완료');
+	// 	console.log({
+	// 		name,
+	// 		nickname,
+	// 	});
+	// });
+
+	// useEffect(() => {
+	// 	console.log('처음 마운트 될떄 만');
+	// 	console.log({
+	// 		name,
+	// 		nickname,
+	// 	});
+	// }, []); // [] -> 처음에만 렌더링 될때만 실행, 업데이트 될때는 실행되지 않음
+
+	useEffect(() => {
+		console.log('name 만 업데이트 될 때');
+		console.log({
+			name,
+		});
+	}, [name]); // [name] -> name 값만 업데이트 될때 실행
+
 	const onChangeName = (e) => {
-		console.log(e.target);
+		// console.log(e.target);
 		setName(e.target.value);
 	};
 
