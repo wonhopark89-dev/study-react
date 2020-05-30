73df88173223880aabb8036f0aab0d9c6a6a9192
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-05-30 23:42:40 +0900

    useState, useRef, useCallback

--- react-spa/src/App.js
+++ react-spa/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -10,9 +10,25 @@ const App = () => {
 		{ id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
 	]);
 
+	// 교유값으로 사용될 id
+	// ref 를 사용하여 변수 담기
+	const nextId = useRef(4);
+	const onInsert = useCallback(
+		(text) => {
+			const todo = {
+				id: nextId.current,
+				text,
+				checked: false,
+			};
+			setTodos(todos.concat(todo));
+			nextId.current += 1; // nextId 1씩 더하기
+		},
+		[todos],
+	);
+
 	return (
 		<TodoTemplate>
-			<TodoInsert />
+			<TodoInsert onInsert={onInsert} />
 			<TodoList todos={todos} />
 		</TodoTemplate>
 	);
