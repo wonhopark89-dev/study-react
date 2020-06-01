10edeadd78212243b82b048a33633b823100bf8c
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-06-01 12:49:30 +0900

    useState funtional update

--- react-spa/src/App.js
+++ react-spa/src/App.js
@@ -3,46 +3,45 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+function createBulkTodos() {
+	const array = [];
+	for (let i = 1; i <= 2500; i++) {
+		array.push({
+			id: i,
+			text: `할 일 ${i}`,
+			checked: false,
+		});
+	}
+	return array;
+}
+
 const App = () => {
-	const [todos, setTodos] = useState([
-		{ id: 1, text: '리액트의 기초 알아보기', checked: true },
-		{ id: 2, text: '컴포넌트 스타일링 해보기', checked: true },
-		{ id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
-	]);
+	const [todos, setTodos] = useState(createBulkTodos);
 
 	// 교유값으로 사용될 id
 	// ref 를 사용하여 변수 담기
-	const nextId = useRef(4);
-	const onInsert = useCallback(
-		(text) => {
-			const todo = {
-				id: nextId.current,
-				text,
-				checked: false,
-			};
-			setTodos(todos.concat(todo));
-			nextId.current += 1; // nextId 1씩 더하기
-		},
-		[todos],
-	);
+	const nextId = useRef(2501);
+	const onInsert = useCallback((text) => {
+		const todo = {
+			id: nextId.current,
+			text,
+			checked: false,
+		};
+		setTodos((todos) => todos.concat(todo));
+		nextId.current += 1; // nextId 1씩 더하기
+	}, []);
 
-	const onRemove = useCallback(
-		(id) => {
-			setTodos(todos.filter((tomato) => tomato.id !== id));
-		},
-		[todos],
-	); 
+	const onRemove = useCallback((id) => {
+		setTodos((todos) => todos.filter((tomato) => tomato.id !== id));
+	}, []);
 
-	const onToggle = useCallback(
-		(id) => {
-			setTodos(
-				todos.map((tomato) =>
-					tomato.id === id ? { ...tomato, checked: !tomato.checked } : tomato,
-				),
-			);
-		},
-		[todos],
-	);
+	const onToggle = useCallback((id) => {
+		setTodos((todos) =>
+			todos.map((tomato) =>
+				tomato.id === id ? { ...tomato, checked: !tomato.checked } : tomato,
+			),
+		);
+	}, []);
 
 	return (
 		<TodoTemplate>
