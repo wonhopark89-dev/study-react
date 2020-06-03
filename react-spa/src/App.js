41e9614a4b8c55acfe080be027b17958f1c681ce
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-06-03 21:02:31 +0900

    Link, sample component for diff path

--- react-spa/src/App.js
+++ react-spa/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 
 const App = () => {
 	return (
 		<div>
+			<ul>
+				<li>
+					<Link to="/">홈</Link>
+				</li>
+				<li>
+					<Link to="/about">소개</Link>
+				</li>
+			</ul>
+			<hr />
 			{/* exact 옵션이 true 여야 경로 일치 문제가 발생하지 않음 */}
 			<Route path="/" component={Home} exact={true} />
-			<Route path="/about" component={About} />
+			<Route path={['/about', '/info']} component={About} />
 		</div>
 	);
 };
