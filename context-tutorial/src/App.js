c9bec4aac2f9880fcc1946a43a9dde03c7e5ea30
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-06-05 21:51:43 +0900

    Provider, change static value

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ColorBox from './components/ColorBox';
+import ColorContext from './contexts/color';
 
+// Priovider 를 통해 기본값을 value 를 꼭 명시해줘야한다.
 const App = () => {
 	return (
 		<div>
-			<ColorBox />
+			<ColorContext.Provider value={{ color: 'red' }}>
+				<div>
+					<ColorBox />
+				</div>
+			</ColorContext.Provider>
 		</div>
 	);
 };
