5c631453ade5c6c8979b42e6001e6830a99c3cd0
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-06-07 20:02:35 +0900

    class componenet code splitting using state

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import notify from './notify';
 
-function App() {
-	const onClick = () => {
-		import('./notify').then((result) => result.default());
+// function App() {
+// 	// function import 는 Promise 객체를 반환한다.
+// 	const onClick = () => {
+// 		import('./notify').then((result) => result.default());
+// 	};
+
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				<img src={logo} className="App-logo" alt="logo" />
+// 				<p onClick={onClick}>Hello React</p>
+// 			</header>
+// 		</div>
+// 	);
+// }
+
+class App extends Component {
+	state = {
+		SplitMe: null,
+	};
+	handleClick = async () => {
+		const loadedModule = await import('./SplitMe');
+		this.setState({ SplitMe: loadedModule.default });
 	};
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p onClick={onClick}>Hello React</p>
-			</header>
-		</div>
-	);
+	render() {
+		const { SplitMe } = this.state;
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p onClick={this.handleClick}>Hello React</p>
+					{SplitMe && <SplitMe />}
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
