79a2551136f51d96c3fd6675fb1c54958a153f2a
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-06-08 23:04:55 +0900

    React.lazy, Suspense

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,45 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import notify from './notify';
 
-// function App() {
-// 	// function import 는 Promise 객체를 반환한다.
-// 	const onClick = () => {
-// 		import('./notify').then((result) => result.default());
+const SplitMe = React.lazy(() => import('./SplitMe'));
+
+function App() {
+	const [visible, setVisible] = useState(false);
+
+	const onClick = () => {
+		// function import 는 Promise 객체를 반환한다.
+		//import('./notify').then((result) => result.default());
+		setVisible(true);
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p onClick={onClick}>Hello React</p>
+				<Suspense fallback={<div>loading...</div>}>
+					{visible && <SplitMe />}
+				</Suspense>
+			</header>
+		</div>
+	);
+}
+
+// class App extends Component {
+// 	state = {
+// 		SplitMe: null,
+// 	};
+// 	handleClick = async () => {
+// 		const loadedModule = await import('./SplitMe');
+// 		this.setState({ SplitMe: loadedModule.default });
 // 	};
 
-// 	return (
-// 		<div className="App">
-// 			<header className="App-header">
-// 				<img src={logo} className="App-logo" alt="logo" />
-// 				<p onClick={onClick}>Hello React</p>
-// 			</header>
-// 		</div>
-// 	);
+// 	render() {
+// 		const { SplitMe } = this.state;
+// 		return (
+// 			<div className="App">
+// 				<header className="App-header">
+// 					<img src={logo} className="App-logo" alt="logo" />
+// 					<p onClick={this.handleClick}>Hello React</p>
+// 					{SplitMe && <SplitMe />}
+// 				</header>
+// 			</div>
+// 		);
+// 	}
 // }
-
-class App extends Component {
-	state = {
-		SplitMe: null,
-	};
-	handleClick = async () => {
-		const loadedModule = await import('./SplitMe');
-		this.setState({ SplitMe: loadedModule.default });
-	};
-
-	render() {
-		const { SplitMe } = this.state;
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p onClick={this.handleClick}>Hello React</p>
-					{SplitMe && <SplitMe />}
-				</header>
-			</div>
-		);
-	}
-}
 
 export default App;
