c5dbeaa48639b18796522e17ac68ee48bd2307f4
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-06-11 22:00:57 +0900

    add loadable package and splitting

---
+++ ssr-recipe/dist/js/2.chunk.js
@@ -0,0 +1,1 @@
+exports.ids=[2],exports.modules={19:function(e,t){e.exports={}},22:function(e,t,n){"use strict";n.r(t);var r=n(0),u=n.n(r),s=(n(19),function(){return u.a.createElement("div",{className:"Red"},"Red")});t.default=function(){return u.a.createElement(s,null)}}};
