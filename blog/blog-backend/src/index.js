5ac48003e52268394772c65b4cf1b9cbce953a1a
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-06-14 23:28:48 +0900

    koa middleware return Promise

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,1 +1,28 @@
-const hello = 'hello';
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.use((ctx, next) => {
+	console.log(ctx.url);
+	console.log(1);
+	if (ctx.query.authorized !== '1') {
+		ctx.status = 401; // Unauthorized
+		return;
+	}
+	next().then(() => {
+		console.log('END');
+	});
+});
+
+app.use((ctx, next) => {
+	console.log(2);
+	next();
+});
+
+app.use((ctx) => {
+	ctx.body = 'hello world';
+});
+
+app.listen(4000, () => {
+	console.log('Listening to port 4000');
+});
