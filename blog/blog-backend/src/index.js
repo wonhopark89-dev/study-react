ac0168ddd5a28986f55cc9d0fe18248502d896ef
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-06-14 23:37:07 +0900

    add koa-router, nodemon

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,27 +1,20 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
 
-app.use((ctx, next) => {
-	console.log(ctx.url);
-	console.log(1);
-	if (ctx.query.authorized !== '1') {
-		ctx.status = 401; // Unauthorized
-		return;
-	}
-	next().then(() => {
-		console.log('END');
-	});
+// 리우터 설정
+router.get('/', (ctx) => {
+	ctx.body = '홈';
 });
 
-app.use((ctx, next) => {
-	console.log(2);
-	next();
+router.get('/about', (ctx) => {
+	ctx.body = '소개';
 });
 
-app.use((ctx) => {
-	ctx.body = 'hello world';
-});
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods);
 
 app.listen(4000, () => {
 	console.log('Listening to port 4000');
