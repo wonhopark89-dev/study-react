523b45e61afba06d46c3c15b8306717bf6dfae0f
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-06-16 23:39:58 +0900

    check post/patch object validation

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,5 +1,6 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
+import Joi from 'joi';
 
 const { ObjectId } = mongoose.Types;
 
@@ -22,6 +23,20 @@ export const checkObjectId = (ctx, next) => {
  */
 
 export const write = async (ctx) => {
+	const schema = Joi.object().keys({
+		// 객체가 다음 필드를 가지고 있음을 검증
+		title: Joi.string().required(), // required() 가 있으면 필수 항목
+		body: Joi.string().required(),
+		tags: Joi.array().items(Joi.string()).required(),
+	});
+	// 검증하고 나서 실패인 경우 에러 처리
+	const result = Joi.validate(ctx.request.body, schema);
+	if (result.error) {
+		ctx.status = 400; // bad request
+		ctx.body = result.error;
+		return;
+	}
+
 	const { title, body, tags } = ctx.request.body;
 	// 인스턴스 객체 생성
 	const post = new Post({
@@ -88,6 +103,21 @@ export const remove = async (ctx) => {
 */
 export const update = async (ctx) => {
 	const { id } = ctx.params;
+	// patch 는 requied()가 필수는 아님( 있는거만 업데이트하면 되니까 )
+	const schema = Joi.object().keys({
+		title: Joi.string(),
+		body: Joi.string(),
+		tags: Joi.array().items(Joi.string()),
+	});
+
+	// 검증하고 실패인 경우 에러 처리
+	const result = Joi.validate(ctx.request.body, schema);
+	if (result.error) {
+		ctx.status = 400; //bad request
+		ctx.body = result.error;
+		return;
+	}
+
 	try {
 		const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
 			new: true, // true 이면 업데이트 된 데이터를 반환. false 이면 업데이트 전의 데이터를 반환
