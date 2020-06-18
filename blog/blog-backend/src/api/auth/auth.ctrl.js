84f5e3358062681f327e48d414f01f2786d48775
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-06-18 23:46:00 +0900

    update sign-up API

--- blog/blog-backend/src/api/auth/auth.ctrl.js
+++ blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -1,15 +1,61 @@
-export const register = async ctx => {
-    // 회원가입
-}
+import Joi from 'joi';
+import User from '../../models/user';
 
-export const login = async ctx => {
-    // 로그인
-}
+/*
+  POST /api/auth/register
+  { 
+      username:"test",
+      password:"1234"
+  }
+ */
+export const register = async (ctx) => {
+	// 회원가입
+	// Request Body 검증
+	const schema = Joi.object().keys({
+		username: Joi.string().alphanum().min(3).max(20).required(),
+		password: Joi.string().required(),
+	});
 
-export const check = async ctx => {
-    // 로그인 상태 확인
-}
+	const result = Joi.validate(ctx.request.body, schema);
+	if (result.error) {
+		ctx.status = 400;
+		ctx.body = result.error;
+		return;
+	}
 
-export const logout = async ctx => {
-    // 로그아웃
-}
+	const { username, password } = ctx.request.body;
+	try {
+		// username 이 이미 존재하는지 확인
+		const exists = await User.findByUsername(username);
+		if (exists) {
+			ctx.status = 400; // Conflict
+			return;
+		}
+
+		const user = new User({
+			username,
+		});
+
+		await user.setPassword(password); // 비밀번호 설정
+		await user.save(); // 데이터베이스에 저장
+
+		// 응답할 데이터에서 hashedPassword 필드 제거
+		const data = user.toJSON();
+		delete data.hashedPassword;
+		ctx.body = data;
+	} catch (err) {
+		ctx.throw(500, err);
+	}
+};
+
+export const login = async (ctx) => {
+	// 로그인
+};
+
+export const check = async (ctx) => {
+	// 로그인 상태 확인
+};
+
+export const logout = async (ctx) => {
+	// 로그아웃
+};
